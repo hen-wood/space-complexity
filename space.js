2d40fd31a01ee83f2dad29266041c712467c4e21
Henry W <henry.woodmansee@gmail.com>
2022-10-24 22:46:55 -0700

    fixed spelling on O(n) function

--- space.js
+++ space.js
@@ -1,6 +1,6 @@
 const sizeof = require('object-sizeof')
 
-function ofOfEnn(n) {
+function ohOfEnn(n) {
     const arr = [];
     for (let i = 0; i < n; i++) {
         arr.push('+');
@@ -19,7 +19,7 @@ function ohOfEnnSquared(n) {
 }
 console.log('O(n)')
 for (let i = 0; i < 10; i++) {
-    console.log(sizeof(ofOfEnn(i)))
+    console.log(sizeof(ohOfEnn(i)))
 }
 console.log('**********************************************************')
 console.log('O(n^2)')
